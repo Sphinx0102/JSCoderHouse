b3dc37c03e8dd00b3b7bfe5b24a0ee35d4feeafa
Sphinx0102 <leo.sebas.lr@gmail.com>
2023-02-08 00:38:23 -0300

    fix logic

--- Segunda-Pre-Entrega/index.js
+++ Segunda-Pre-Entrega/index.js
@@ -1,42 +1,125 @@
-class movie{
-    constructor(nombre, valoracionIndididual){
+class Movie{
+    constructor(nombre, valoracionIndididual, valoracion, cantidadValoracion){
         this.nombre = nombre;
-        this.valoracion += valoracionIndididual;
-        this.cantidadValoracion;
+        this.valoracionIndididual= valoracionIndididual;
+        this.valoracion = valoracion;
+        this.cantidadValoracion = cantidadValoracion;
     }
 }
-let cartelera = [{nombre:"Avatar: El Camino del Agua", valoracion:0, cantidadValoracion: 0},
-                {nombre:"The Wale", valoracion:0, cantidadValoracion: 0},
-                {nombre:"Llaman a la puerta", valoracion:0, cantidadValoracion: 0},
-                {nombre:"M3gan", valoracion:0, cantidadValoracion: 0},
-                {nombre:"Gato Con botas: El Ultimo Deseo", valoracion:0, cantidadValoracion: 0}];
 
-let opcion = prompt('Bienvenido al espacio de FeedBack de peliculas en cartelera.',
-                    'Selecciona una de las siguientes peliculas a valorar o escribe "Salir" para finalizar la devolucion',
-                    '1- Avatar: El Camino del Agua',
-                    '2- The Wale',
-                    '3- Llaman a la puerta',
-                    '4- M3gan',
-                    '5- Gato Con botas: El Ultimo Deseo');
+let Avatar = new Movie("Avatar: El Camino del Agua", 0, 0, 0);
+let Wale = new Movie("The Wale", 0, 0, 0);
+let Llaman = new Movie("Llaman a la puerta", 0, 0, 0);
+let Megan = new Movie("M3gan", 0, 0, 0);
+let Gato = new Movie("Gato Con botas: El Ultimo Deseo", 0, 0, 0);
 
+let cartelera = [Avatar, Wale, Llaman, Megan, Gato];
+
+let opcion= parseInt(prompt(`Bienvenido a Play Cinema. Ingrese alguna de las opciones para operar:
+                    1-Ver Cartelera
+                    2-Ver Valoraciones de la cartelera
+                    3-Realizar una valoracion de una pelicula
+                    4-Salir`));
+
+
+function printCartelera(cartelera){
+    let mensaje = "Cartelera: \n"
+    cartelera.forEach(movie => {
+        mensaje +=`Pelicula: ${movie.nombre}`;
+    });
+    alert(mensaje);
+}
+
+function valoracionTotal(cartelera){
+    let mensaje = "Cartelera: \n"
+    cartelera.forEach(movie => {
+        mensaje +=`Pelicula: ${movie.nombre}, Valoracion ${movie.valoracion}`;
+    });
+    alert(mensaje);
+}
 
 function valorateMovie(movie, valoracionIndididual){
-    
+    movie.valoracionIndididual = valoracionIndididual;
+    movie.cantidadValoracion += 1;
+    movie.valoracion = ((movie.valoracion * movie.cantidadValoracion) + movie.valoracionIndididual) / (movie.cantidadValoracion + 1);
 }
 
-
-while(opcion != 'Salir'){
+while(opcion != 4){
     switch(opcion){
-        case '1':{
-            let valoracion= parseFloat(prompt("Ingrese en una escala del 1 al 10 su valoracion:"));
-            valorateMovie(cartelera[0], valoracion);
+        case 1:{
+            printCartelera(cartelera);
+            break;
+        }
+        case 2:{
+            valoracionTotal(cartelera);
+            break;
+        }
+        case 3:{
+            let opcion2 = prompt(`Bienvenido al espacio de FeedBack de peliculas en cartelera.
+                            Selecciona el numero correspondiente de la pelicula a valorar o opcion 6 para finalizar la devolucion
+                            1- Avatar: El Camino del Agua
+                            2- The Wale
+                            3- Llaman a la puerta
+                            4- M3gan
+                            5- Gato Con botas: El Ultimo Deseo`);
+            
+                                        
+            while(opcion2 != 6){
+                switch(opcion2){
+                    case 1:{
+                        let valoracion= parseFloat(prompt("Ingrese en una escala del 1 al 10 su valoracion:"));
+                        valorateMovie(Avatar, valoracion);
+                        alert(`la valoracion actual de la pelicula: ${Avatar.nombre} es: ${Avatar.valoracion}`);
+                        break;
+                    }
+                    case 2:{
+                        let valoracion= parseFloat(prompt("Ingrese en una escala del 1 al 10 su valoracion:"));
+                        valorateMovie(Wale, valoracion);
+                        alert(`la valoracion actual de la pelicula: ${Wale.nombre} es: ${Wale.valoracion}`);
+                        break;
+                    }
+                    case 3:{
+                        let valoracion= parseFloat(prompt("Ingrese en una escala del 1 al 10 su valoracion:"));
+                        valorateMovie(Llaman, valoracion);
+                        alert(`la valoracion actual de la pelicula: ${Llaman.nombre} es: ${Llaman.valoracion}`);
+                        break;
+                    }
+                    case 4:{
+                        let valoracion= parseFloat(prompt("Ingrese en una escala del 1 al 10 su valoracion:"));
+                        valorateMovie(Megan, valoracion);
+                        alert(`la valoracion actual de la pelicula: ${Megan.nombre} es: ${Megan.valoracion}`);
+                        break;
+                    }
+                    case 5:{
+                        let valoracion= parseFloat(prompt("Ingrese en una escala del 1 al 10 su valoracion:"));
+                        valorateMovie(Gato, valoracion);
+                        alert(`la valoracion actual de la pelicula: ${Gato.nombre} es: ${Gato.valoracion}`);
+                        break;
+                    }
+                    default:{
+                        alert("Opcion Ingresada no correspondiente. Intente de nuevo");
+                        break;
+                    }
+                }
+                opcion2 = prompt(`Bienvenido al espacio de FeedBack de peliculas en cartelera.
+                                    Selecciona el numero correspondiente de la pelicula a valorar o opcion 6 para finalizar la devolucion
+                                    1- Avatar: El Camino del Agua
+                                    2- The Wale
+                                    3- Llaman a la puerta
+                                    4- M3gan
+                                    5- Gato Con botas: El Ultimo Deseo`);
+            }
+            break;
+        }
+        default:{
+            alert("Opcion Ingresada no correspondiente. Intente de nuevo");
+            break;
         }
     }
-    opcion = prompt('Bienvenido al espacio de FeedBack de peliculas en cartelera.',
-                    'Selecciona una de las siguientes peliculas a valorar o escribe "Salir" para finalizar la devolucion',
-                    '1- Avatar: El Camino del Agua',
-                    '2- The Wale',
-                    '3- Llaman a la puerta',
-                    '4- M3gan',
-                    '5- Gato Con botas: El Ultimo Deseo');
+    opcion= parseInt(prompt(`Bienvenido a Play Cinema. Ingrese alguna de las opciones para operar:
+                    1-Ver Cartelera
+                    2-Ver Valoraciones de la cartelera
+                    3-Realizar una valoracion de una pelicula
+                    4-Salir`));
+
 }
